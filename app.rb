a54fb8bad61a71481ff9ccbbb96f6b621f17d55a
Yunnie Yunick <yunnieyunick@gmail.com>
2019-10-17 00:35:58 +0900

    add multiple search word

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
+search_word = ["Google", "yahoo"]
+
 doc = Nokogiri::HTML(open('https://www.google.com'))
 
 doc.xpath('//*[@id="prm"]/div/a').each do |link|
-  puts link.content
+  if search_word.any? {|w| link.content.include?(w)}
+    #send mail
+    puts link.content
+  end
 end
