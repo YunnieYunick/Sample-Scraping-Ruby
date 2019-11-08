b9a518e26b62a29770de4b7a2f0b5a5a3a6316d8
Yunnie Yunick <yunnieyunick@gmail.com>
2019-11-09 01:21:54 +0900

    fix multiple site

--- app.rb
+++ app.rb
@@ -2,32 +2,34 @@ require 'nokogiri'
 require 'open-uri'
 require 'mail'
 
-search_word = ["Google", "Designer"]
-
-
-mail = Mail.new do
-  from    'FROM_MAIL'
-  to      'TO_MAIL'
-  subject 'Scraping Test'
-  body 'scraping'
-end
-
+search_word = ["search_word1", "search_word2"]
+target_site = {
+                'target_site1' => 'selector1',
+                'target_site2' => 'selector2'
+              }
+mail_to = 'mail_to_address'
 options = { :address               => 'smtp.gmail.com',
             :port                  => 587,
             :domain                => 'gmail.com',
-            :user_name             => 'FROM_MAIL',
-            :password              => 'FROM_MAIL_PW',
+            :user_name             => 'mail_user',
+            :password              => 'mail_user_pw',
             :authentication        => :plain,
-            :enable_starttls_auto  => true  }
+            :enable_starttls_auto  => true  
+          }
 
-
-
-doc = Nokogiri::HTML(open('URL'))
-
-doc.xpath('XPATH').each do |link|
-  if search_word.any? {|w| link.content.include?(w)}
-    mail.charset = 'utf-8'
-    mail.delivery_method(:smtp, options)
-    mail.deliver
+target_site.each do |key, value|
+  doc = Nokogiri::HTML(open(key))
+  doc.css(value).each do |link|
+    if search_word.any? {|w| link.content.include?(w)}
+      mail = Mail.new do
+        from    options[:user_name ]
+        to      mail_to
+        subject "Scraping #{key}"
+        body "Get: #{link.content}"
+      end
+      mail.charset = 'utf-8'
+      mail.delivery_method(:smtp, options)
+      mail.deliver
+    end
   end
 end
